dccb5909fc8b6496413bf877709d89f3f51288ec
ForeverFloripa <andre_monso@yahoo.com.br>
2021-06-21 02:45:51 -0300

    .

--- JavaFxUI-MarketFruits/src/controller/MarketController.java
+++ JavaFxUI-MarketFruits/src/controller/MarketController.java
@@ -1,5 +1,32 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
 public class MarketController {
+	
+	@FXML
+    private VBox chosenFruitCard;
+
+    @FXML
+    private Label fruitNameLabel;
+
+    @FXML
+    private Label fruitPriceLabel;
+
+    @FXML
+    private ImageView fruitImg;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private GridPane grid;
+
+
 	
 }
