918862fd640a63591bedaf7bb8c878d654320e5a
ForeverFloripa <andre_monso@yahoo.com.br>
2021-06-21 02:37:33 -0300

    item criado

---
+++ JavaFxUI-MarketFruits/src/controller/ItemController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.image.ImageView;
+
+public class ItemController {
+	@FXML
+	private Label nameLabel;
+	@FXML
+	private Label priceLabel;
+	@FXML
+	private ImageView img;
+
+}
